dff2ccea619728d64e0c8365b91d81371fda0503
Gabriel <mzsgabriel@gmail.com>
2024-03-25 23:20:58 -0300

    Implementação Cliente, Funcionário e Produto

---
+++ scr/mod_produto/Produto.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from decimal import Decimal
+
+class Produto(BaseModel):
+    id_produto: int = None
+    nome: str
+    descricao : str
+    foto: bytes
+    valor: Decimal
+    
